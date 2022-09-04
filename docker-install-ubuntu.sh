af2c6e77d32fbbb3a825090b3bb3ea86c734d4e9
mgpai <64056966+mgpai22@users.noreply.github.com>
2022-09-04 10:32:05 -0400

    Update docker-install-ubuntu.sh

--- docker-install-ubuntu.sh
+++ docker-install-ubuntu.sh
@@ -7,14 +7,13 @@ if [ -x "$(command -v docker)" ]; then
     echo "docker is installed!"
 else
     echo "Installing docker..."    
-    apt update
-    apt install apt-transport-https ca-certificates curl software-properties-common -y
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    apt update
-    apt install docker-ce -y
-    sudo chmod 666 /var/run/docker.sock
-    usermod -aG docker ${USER}
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo apt update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -aG docker $USER
 fi
